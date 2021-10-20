6d862ec0c5a3bb60b2f4a73c277f341c5914bdd5
Charity Rogers <rogerscl116@gmail.com>
2021-10-20 12:37:42 -0500

    add ball and brick intersection

--- src/com/brickBreaker/Gameplay.java
+++ src/com/brickBreaker/Gameplay.java
@@ -1,5 +1,7 @@
 package com.brickBreaker;
 
+import org.w3c.dom.css.Rect;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -68,6 +70,35 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener {
         if (play) {
             if (new Rectangle(ballposX, ballposY, 20, 20).intersects(new Rectangle(playerX, 550, 100, 8))) {
                 ballYdir = -ballYdir;
+            }
+
+            A: for (int i = 0; i < map.map.length; i++) {
+                for (int j = 0; j < map.map[0].length; j++) {
+                    if (map.map[i][j] > 0) {
+                        int brickX = j * map.brickWidth + 80;
+                        int brickY = i * map.brickHeight + 50;
+                        int brickWidth = map.brickWidth;
+                        int brickHeight = map.brickHeight;
+
+                        Rectangle rect = new Rectangle(brickX, brickY, brickWidth, brickHeight);
+                        Rectangle ballRect = new Rectangle(ballposX, ballposY, 20, 20);
+                        Rectangle brickRect = rect;
+
+                        if (ballRect.intersects(brickRect)) {
+                            map.setBrickValue(0, i, j);
+                            totalBricks--;
+                            score += 5;
+
+                            if (ballposX + 19 <= brickRect.x || ballposX + 1 >= brickRect.x + brickRect.width) {
+                                ballXdir = -ballXdir;
+                            } else {
+                                ballYdir = -ballYdir;
+                            }
+
+                            break A;
+                        }
+                    }
+                }
             }
 
             ballposX += ballXdir;
--- src/com/brickBreaker/MapGenerator.java
+++ src/com/brickBreaker/MapGenerator.java
@@ -17,12 +17,11 @@ public class MapGenerator {
         brickWidth = 540/col;
         brickHeight = 150/row;
     }
-
     public void draw(Graphics2D g) {
         for (int i = 0; i < map.length; i++) {
             for (int j = 0; j < map[0].length; j++) {
                 if (map[i][j] > 0) {
-                    g.setColor(Color.white);
+                    g.setColor(Color.red);
                     g.fillRect(j * brickWidth + 80, i * brickHeight + 50, brickWidth, brickHeight);
 
                     g.setStroke(new BasicStroke(3));
@@ -32,5 +31,8 @@ public class MapGenerator {
                 }
             }
         }
+    }
+    public void setBrickValue(int value, int row, int col) {
+        map[row][col] = value;
     }
 }
